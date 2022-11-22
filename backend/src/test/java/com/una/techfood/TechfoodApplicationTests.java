5160c68b08da5f57eb4e6f1cace972fe1686eb67
Pedro Henrique <pedrohnleal@gmail.com>
2022-11-22 13:11:30 -0300

    Project created

---
+++ backend/src/test/java/com/una/techfood/TechfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.una.techfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
